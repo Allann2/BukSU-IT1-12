ec462e5f56d3cb493630c43809f5fd8aa9692dad
Ladera, Allan Keith L <73632549+Allann2@users.noreply.github.com>
2020-11-10 08:41:12 +0800

    Add files via upload

---
+++ NegativePositiveDeterminer.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int num;
+    printf("Enter a Number to be Determined: ");
+    scanf(" %d", &num);
+
+    if (num > 0){
+        printf("The number you entered is a POSITIVE number");
+
+        }
+
+        else{
+            printf
+            ("The number you entered is a NEGATIVE number");
+
+    }
+
+
+    return 0;
+}
